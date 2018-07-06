d6b41e720a4676d701dd6d22bd45fd144db414d7
Xianwen Peng <fallenpxw@gmail.com>
2018-07-06 00:08:37 -0400

    service getNews Trial

--- web_server/backend_server/service.py
+++ web_server/backend_server/service.py
@@ -1,4 +1,10 @@
 import pyjsonrpc
+import os
+import sys
+
+# import common package in parent directory
+
+sys.path.apend(os.path.join(os.path.dirname(__file__), './', 'utils'))
 
 SERVER_HOST = 'localhost'
 SERVER_PORT = 4040
@@ -9,6 +15,11 @@ class RequestHandler(pyjsonrpc.HttpRequestHandler):
     def add(self, a, b):
         print "add is called with %d and %d" % (a, b)
         return a + b
+
+    @pyjsonrpc.rpcmethod
+    def getNews(self):
+        db = mongodb_client.get_db()
+        return list(db['news'].find())
 
 http_server = pyjsonrpc.ThreadingHttpServer(
     server_address = (SERVER_HOST, SERVER_PORT),
