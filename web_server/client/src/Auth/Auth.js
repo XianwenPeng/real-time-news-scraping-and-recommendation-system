0fc356bb04e8eadc14c418a8d48a95f31a67e122
Xianwen Peng <fallenpxw@gmail.com>
2018-07-14 20:48:06 -0400

    Auth Login SignUp

---
+++ web_server/client/src/Auth/Auth.js
@@ -0,0 +1,27 @@
+class Auth {
+  /*
+    Authenticate a User, save a token string in Local Storage
+  */
+  static authenticateUser(token, email) {
+    localStorage.setItem('token', token);
+    localStorage.setItem('email', email);
+  }
+
+  static isUserAuthenticated() {
+    return localStorage.getItem('token') !== null;
+  }
+
+  static deauthenticateUser() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('email');
+  }
+
+  static getToken() {
+    return localStorage.getItem('token');
+  }
+
+  static getEmail() {
+    return localStorage.getItem('email');
+  }
+
+}
